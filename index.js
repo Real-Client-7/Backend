78bf1d5868c242609514d991480852243df8fbbf
zahraa mazloum <zahraamazloum2001@gmail.com>
2023-04-13 13:10:14 +0300

    Patient CRUD

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {config} from 'dotenv'
 import connectDB from "./config/db.js"
 import bodyParser from "body-parser"
 import Appointment from "./routes/AppoitmentRoute.js"
-
+import patientRouter from "./routes/patientRoutes.js"
 import adminRouter from "./routes/adminRoute.js"
 
 config()
@@ -15,10 +15,13 @@ const app = express()
 
 const port = process.env.PORT || 5000;
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use(morgan('tiny'))
 
 app.use("/admin" ,adminRouter)
 app.use("/appointment" ,Appointment)
+app.use("/patient" ,patientRouter)
+
 
 app.listen(port,(req , res)=>{
     console.log(`server listen on port ${port}`)
