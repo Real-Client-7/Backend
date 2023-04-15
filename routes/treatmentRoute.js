8078623426c2197320d007766e75d027821968e0
AhmadBreiss <ahmatobreiss@gmail.com>
2023-04-15 22:08:45 +0300

    created routes of Treatment

---
+++ routes/treatmentRoute.js
@@ -0,0 +1,14 @@
+import express from "express"
+import Treatment from "../controllers/treatmentController.js"
+
+const router =express.Router()
+
+router.post('/' , Treatment.addTreatment)
+router.get('/',Treatment.getTreatment)
+router.get('/:id',Treatment.getTreatmentByID)
+router.delete('/:id' , Treatment.deleteTreatment)
+router.put('/:id',Treatment.updateTreatment)
+
+
+export default router;
+
