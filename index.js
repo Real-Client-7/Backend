bb0f3538119fe81f9171ba7c8ce1844777a5f778
Mazen El Ali <mazenelali>
2023-05-07 02:03:07 +0300

    edit

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(morgan('tiny'))
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:4600" , "https://bassam-monla-ycid.onrender.com"]
+}));
   app.use("/admin" ,adminRouter)
 app.use("/appointment" ,Appointment)
 app.use("/patient" ,patientRouter)
